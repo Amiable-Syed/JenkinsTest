642ec3db204afca9dca96f6bbc430ccd33696a0c
Syed Saqib <saqisyed143@gmail.com>
2018-03-30 19:36:43 +0500

    Update HelloTest.java

--- test/HelloTest.java
+++ test/HelloTest.java
@@ -1,7 +1,8 @@
 import junit.framework.TestCase;
 
 public class HelloTest extends TestCase {
-    public void test1() {}
+    public void test1() {
+                new Hello.foo();}
     public void test2() {
         new Hello.Foo();
     }
